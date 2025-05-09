92ebf62af9b9c9181b2de524188ee272b595eb87
AlejandroOFDY <BB_Rae@hotmail.com>
2025-05-09 14:20:52 -0300

    Actualización, solo se agregó el switch pero aún no es funcional

---
+++ app.js
@@ -0,0 +1,22 @@
+function convertCurrency(pesos) {
+   return resultado = (pesos / 1300)
+}
+
+switch (numero = parseInt(prompt("1) Divisa (ARS --> USD)\n2) Salir"))) {
+    case 1:
+        parseInt(prompt(convertCurrency("Ingrese el monto que desea convertir: ")))
+        alert(resultado)
+        break;
+    case 2:
+        alert("Adios")
+        break;
+    default:
+        alert("Opciones inválidas, nuevamente") 
+    }
+
+history = {
+    tipo: "divisa",
+    entrada: "1300 ARS",
+    salida: "1 USD",
+    fecha:
+}
