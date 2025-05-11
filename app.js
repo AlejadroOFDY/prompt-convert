e3e31c529e56830d1f89dbc2755432c45da554f1
Alejandro <BB_rae@hotmail.com>
2025-05-11 20:26:33 -0300

    Agregado el registro como pedía la consigna

--- app.js
+++ app.js
@@ -1,26 +1,33 @@
 
+let history = []
 
 function convertCurrency(pesos) {
-    return resultado = ((pesos / 1300).toFixed(2) + " dólares")
+    resultado = ((pesos / 1300).toFixed(2))
+    let objeto = {
+    tipo: "divisa",
+    entrada: [pesos],
+    salida: [resultado],
+    fecha: new Date().toLocaleString()
+}
+    history.push(objeto)
+    return resultado
 }
 
 let numero;
+let salida = true
 
-while (true) {
+while (salida) {
     numero = parseFloat(prompt("1) Divisa (ARS     -->USD)\n2) Salir"))
     switch (numero) {
         case 1:
             convertCurrency(parseFloat(prompt("Ingrese el monto que desea convertir: ")))
             alert(resultado)
-            alert(pesos)
             break;
         case 2:
-            alert("hola")
+            console.log(history)
+            salida = false
             break;
         default:
             alert("Opciones inválidas, nuevamente") 
     }
 }
-
-
-
